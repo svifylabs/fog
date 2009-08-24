d070fc0170ad3421559ffab809a2771416ba1cfd
kobalicek.petr <kobalicek.petr@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2009-08-24 23:57:50 +0000

    Added Rasterizer stub

---
+++ Fog/Fog/Graphics/Rasterizer.cpp
@@ -0,0 +1,29 @@
+// [Fog/Graphics Library - C++ API]
+//
+// [Licence]
+// MIT, See COPYING file in package
+
+// [Precompiled Headers]
+#ifdef FOG_PRECOMP
+#include FOG_PRECOMP
+#endif
+
+// [Dependencies]
+#include <Fog/Graphics/Error.h>
+#include <Fog/Graphics/Rasterizer.h>
+
+namespace Fog {
+
+// ============================================================================
+// [Fog::Rasterizer]
+// ============================================================================
+
+Rasterizer::Rasterizer()
+{
+}
+
+Rasterizer::~Rasterizer()
+{
+}
+
+} // Fog namespace
