ffc7e50283ed024c46710608a0ad354deb096a6e
stefanvt@gmx.de <stefanvt@gmx.de@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2010-04-13 10:50:17 +0000

    add missing files

---
+++ branches/stefanvt/Fog/Fog/Gui/Animation.cpp
@@ -0,0 +1,136 @@
+// [Fog-Gui Library - Public API]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Precompiled Headers]
+#if defined(FOG_PRECOMP)
+#include FOG_PRECOMP
+#endif // FOG_PRECOMP
+
+// [Dependencies]
+#include <Fog/Gui/Animation.h>
+#include <Fog/Core/Application.h>
+#include <Fog/Core.h>
+
+FOG_IMPLEMENT_OBJECT(Fog::Animation)
+FOG_IMPLEMENT_OBJECT(Fog::AnimationDispatcher)
+FOG_IMPLEMENT_OBJECT(Fog::WidgetAnimation)
+FOG_IMPLEMENT_OBJECT(Fog::WidgetOpacityAnimation)
+
+namespace Fog {
+
+Animation::Animation(uint32_t t) : _type(t), _step(10), _duration(TimeDelta::fromMilliseconds(200)), _position(0.0f) {
+  
+}
+
+Animation::~Animation() {
+
+}
+
+void Animation::onTimer(TimerEvent* e)
+{
+  if(_position >= 1.0) {
+    //Animation is finished! should not happen
+    return;
+  }
+
+  bool finished = false;
+
+  if(_type == ANIMATION_FIXED_STEP) {
+    if (_direction == ANIMATION_FORWARD)
+    {
+      _position += _step;
+      if (_position >= 1.0f) { _position = 1.0f; finished = true; }
+    }
+    else
+    {
+      _position -= _step;
+      if (_position <= 0.0f) { _position = 0.0f; finished = true; }
+    }
+  } else {
+    //dynamically calculate position based on time elapsed since start    
+    TimeTicks now = TimeTicks::highResNow();
+    if(now >= _endtime) {
+      if(_direction == ANIMATION_BACKWARD) {
+        _position = 0.0f;
+      } else {
+        _position = 1.0f;
+      }
+      finished = true;
+    } else {
+      TimeDelta delta = now - _starttime;
+      //in _step the value for one ms is stored
+      _position = delta.inMilliseconds() * _step;
+
+      if(_position > 1.0f) {
+        _position = 1.0f;
+      }
+
+      if(_direction == ANIMATION_BACKWARD) {
+        _position = 1.0f - _position;
+      }
+    }
+  }
+
+  // Call onAnimation() and onFinished() if animation is finished.
+  AnimationEvent event;
+
+  event._type = EVENT_ANIMATION_STEP;
+  sendEvent(&event);
+
+  if (finished)
+  {
+    event._type = EVENT_ANIMATION_FINISHED;
+    sendEvent(&event);
+
+    //should we call this here? Or call it at destructor?
+    Application::getInstance()->getAnimationDispatcher()->removeAnimation(this);
+  }
+}
+
+void AnimationDispatcher::onTimer(TimerEvent* e) {
+  //notify all registered handler
+  sendEvent(e);
+  //or directly call doUpdate() ??  
+
+  while(_finished.getLength() > 0) {
+    Animation* e = _finished.takeFirst();
+    //who is responsible to destruct?
+    //current answer: call destroy on Animation
+    removeListener(e);
+    e->destroy();
+  }
+
+ Application::getInstance()->getGuiEngine()->update();
+}
+
+AnimationDispatcher::AnimationDispatcher(TimeDelta ms) : _count(0) {
+  _timer.setInterval(ms);
+  _timer.addListener(EVENT_TIMER,this,&AnimationDispatcher::onTimer);
+  //No local onTimer-Event
+  //The Time Events are dispatched directly to AnimationInstances
+}
+
+void AnimationDispatcher::addAnimation(Animation* a) {
+  addListener(EVENT_TIMER,a,&Animation::onTimer);
+  ++_count;
+  if(_count == 1) {
+    //So the Timer is only running if a animation is in there
+    _timer.start();
+  }
+
+  //To set up start parameter, or should we call this on first time event 
+  //or maybe on construction?
+  a->onStart();
+}
+
+void AnimationDispatcher::removeAnimation(Animation* e) {
+  _finished.append(e);  
+  --_count;
+  if(_count == 0) {
+    _timer.stop();
+  }
+}
+
+}
---
+++ branches/stefanvt/Fog/Fog/Gui/Animation.h
@@ -0,0 +1,164 @@
+// [Fog-Gui Library - Public API]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Guard]
+#ifndef _FOG_GUI_ANIMATION_H
+#define _FOG_GUI_ANIMATION_H
+
+#include <Fog/Build/Build.h>
+
+#include <Fog/Gui/Constants.h>
+#include <Fog/Gui/Event.h>
+#include <Fog/Gui/GuiEngine.h>
+#include <Fog/Gui/Widget.h>
+#include <Fog/Core/Timer.h>
+#include <Fog/Core/Time.h>
+
+namespace Fog {
+struct Application;
+
+struct FOG_API Animation : public Object
+{
+  FOG_DECLARE_OBJECT(Animation, Object)
+
+  Animation(uint32_t t=ANIMATION_FIXED_STEP);
+  virtual ~Animation();
+
+  void onStart() {
+    //called from Dispatcher after animation was inserted into listener queue
+    _starttime = TimeTicks::highResNow();
+    if(_type == ANIMATION_FIXED_TIME) {
+      _endtime = _starttime + getDuration();
+      //store in _step the value for 1 ms for easy calculate of _position
+      //based on elapsed time since start
+      _step = (float) (1.0f / getDuration().inMilliseconds());
+    }
+  }
+
+  virtual void onAnimation(AnimationEvent* e) {
+    if(e->_type == EVENT_ANIMATION_STEP) {
+      onStep(e);
+    } else {
+      onFinished(e);
+    }
+  }
+
+  virtual void onTimer(TimerEvent* e);
+
+  virtual void onStep(AnimationEvent* e) {
+
+  }
+  virtual void onFinished(AnimationEvent* e) {
+
+  }
+
+  FOG_INLINE float getStep() const { return _step; }
+  FOG_INLINE float getFps() const { return _fps; }
+  FOG_INLINE uint getDirection() const { return _direction; }
+  FOG_INLINE TimeDelta getDuration() const { return _duration; }
+
+  void setStep(float step) { _type = ANIMATION_FIXED_STEP; _step = step; }
+  void setFps(float fps);
+  void setDirection(uint direction) { _direction = direction; }
+  void setDuration(TimeDelta duration) { _type = ANIMATION_FIXED_TIME;  _duration = duration; }
+
+  FOG_EVENT_BEGIN()
+    FOG_EVENT_DEF(EVENT_ANIMATION           , onAnimation        , AnimationEvent     , OVERRIDE)
+  FOG_EVENT_END()
+
+protected:
+  float _position;
+  float _step;
+  float _fps;
+
+  TimeDelta _duration;
+
+  uint32_t _direction;
+  uint32_t _type;
+
+  TimeTicks _starttime;
+  TimeTicks _endtime;
+};
+
+
+struct FOG_API AnimationDispatcher : public Object {
+  FOG_DECLARE_OBJECT(AnimationDispatcher, Object)
+
+  AnimationDispatcher(TimeDelta ms=TimeDelta::fromMilliseconds(20));
+  ~AnimationDispatcher() {
+
+  }
+
+  //add an Animation to dispatcher
+  //the dispatcher will take the ownership of the animation-object!
+  void addAnimation(Animation* a);
+
+  //remove an Animation from dispatcher
+  //the dispatcher will destroy the object itself
+  void removeAnimation(Animation* a);
+
+  virtual void onTimer(TimerEvent* e);
+
+private:
+  int _count;   //number of registered animations
+  Timer _timer; //Timer object for animations
+
+  List<Animation*> _finished;
+};
+
+
+struct FOG_API WidgetAnimation : public Animation
+{
+  FOG_DECLARE_OBJECT(WidgetAnimation, Animation)
+
+  WidgetAnimation(Widget* widget) : _widget(widget) {
+
+  }
+  virtual ~WidgetAnimation() {
+
+  }
+
+  FOG_INLINE Widget* getWidget() const { return _widget; }
+
+  void setWidget(Widget* widget) { _widget = widget; };
+
+protected:
+  Widget* _widget;
+};
+
+
+struct FOG_API WidgetOpacityAnimation : public WidgetAnimation
+{
+  FOG_DECLARE_OBJECT(WidgetOpacityAnimation, WidgetAnimation)
+
+  WidgetOpacityAnimation(Widget* widget) : WidgetAnimation(widget) {
+
+  }
+
+  virtual ~WidgetOpacityAnimation() {
+
+  }
+
+  virtual void onStep(AnimationEvent* e) {
+    float opacity = _startOpacity + (_endOpacity - _startOpacity) * _position;
+    if (_widget) {
+      _widget->setTransparency(opacity);        
+    }
+  }
+
+  FOG_INLINE float getStartOpacity() const { return _startOpacity; }
+  FOG_INLINE float getEndOpacity() const { return _endOpacity; }
+
+  void setStartOpacity(float startOpacity) { _startOpacity = startOpacity; }
+  void setEndOpacity(float endOpacity) { _endOpacity = endOpacity; }
+
+protected:
+  float _startOpacity;
+  float _endOpacity;
+};
+
+}
+
+#endif
