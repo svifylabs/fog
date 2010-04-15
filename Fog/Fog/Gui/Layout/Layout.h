0057e8ceac881f8f3d523fb8219f238c2f5e47c4
stefanvt <stefanvt@gmx.de>
2010-04-15 19:48:21 +0000

    - first version of basic layout engine

--- Fog/Fog/Gui/Layout/Layout.h
+++ Fog/Fog/Gui/Layout/Layout.h
@@ -8,7 +8,7 @@
 #define _FOG_GUI_LAYOUT_LAYOUT_H
 
 // [Dependencies]
-#include <Fog/Core/Event.h>
+#include <Fog/Gui/Event.h>
 #include <Fog/Core/Object.h>
 #include <Fog/Graphics/Geometry.h>
 #include <Fog/Gui/Constants.h>
@@ -22,6 +22,10 @@ namespace Fog {
 // ============================================================================
 // [Fog::Layout]
 // ============================================================================
+
+const int DEFAULT_LAYOUT_SPACING = 6;
+const int DEFAULT_WIDGET_MARGIN = 9;
+const int DEFAULT_WINDOW_MARGIN = 11;
 
 //! @brief Base class for all layouts.
 struct FOG_API Layout : public LayoutItem
@@ -72,29 +76,65 @@ struct FOG_API Layout : public LayoutItem
   FOG_INLINE SizeConstraint getSizeConstraint() const { return _constraint; }
 
   FOG_INLINE void setEnabled(bool enable) { _enabled = enable; }
-  FOG_INLINE bool getEnabled() const { return _enabled; }
+  FOG_INLINE bool isEnabled() const { return _enabled; }
 
-  FOG_INLINE void setContentsMargins(int left, int right, int top, int bottom) { _contentmargins.set(left,right,top,bottom); }
-  FOG_INLINE void setContentsMargins(const IntMargins& m) { _contentmargins.set(m); }
-  FOG_INLINE IntMargins getContentsMargins() const { return _contentmargins; }
+  FOG_INLINE int calcMargin(int margin) const;
 
-  FOG_INLINE IntRect getContentsRect() const {
+  FOG_INLINE void setContentsMargins(int left, int right, int top, int bottom) { 
+    IntMargins m = _contentmargins;
+    _contentmargins.left = calcMargin(left);
+    _contentmargins.right = calcMargin(right);
+    _contentmargins.top = calcMargin(top);
+    _contentmargins.bottom = calcMargin(bottom);
+    if(_contentmargins.eq(m))
+      return;
+
+    _margininvalid = false;
+    invalidateLayout();
+  }
+
+  FOG_INLINE void setContentsMargins(const IntMargins& m) { 
+    setContentsMargins(m.left,m.right,m.top,m.bottom);
+  }
+  
+  FOG_INLINE IntMargins getContentsMargins() { 
+    if(_margininvalid) 
+      setContentsMargins(_contentmargins); 
+    return _contentmargins; 
+  }
+
+  FOG_INLINE IntRect getContentsRect() {
+    if(_margininvalid) 
+      setContentsMargins(_contentmargins);
     return _rect.adjusted(+_contentmargins.left, +_contentmargins.top, -_contentmargins.right, -_contentmargins.bottom);
   }
 
-  virtual int getSpacing() const { return _spacing; }
+  virtual int getSpacing() const { 
+    if (_spacing >=0) {
+      return _spacing;
+    } else {
+      // arbitrarily prefer horizontal spacing to vertical spacing
+      //return qSmartSpacing(this, QStyle::PM_LayoutHorizontalSpacing);
+      if (!_parentItem) {
+        return -1;
+      } else if (_parentItem->isWidget()) {
+        return DEFAULT_LAYOUT_SPACING;
+      } else {
+        FOG_ASSERT(_parentItem->isLayout());
+        return static_cast<Layout *>(_parentItem)->getSpacing();
+      }
+
+      return 0;
+    }  
+  }
   virtual void setSpacing(int spacing) { _spacing = spacing; }
   
 
   //From LayoutItem  
-  virtual uint32_t getLayoutExpandingDirections() const {
-    return ORIENTATION_HORIZONTAL | ORIENTATION_VERTICAL;
-  }
+  virtual uint32_t getLayoutExpandingDirections() const { return ORIENTATION_HORIZONTAL | ORIENTATION_VERTICAL; }
   virtual bool hasLayoutHeightForWidth() const { return false; } 
-
   virtual IntSize getLayoutMinimumSize() const { return IntSize(WIDGET_MIN_SIZE, WIDGET_MIN_SIZE); };
   virtual IntSize getLayoutMaximumSize() const { return IntSize(WIDGET_MAX_SIZE, WIDGET_MAX_SIZE); };
-
   virtual void setLayoutGeometry(const IntRect& r) {
     _rect = r;
   }
@@ -109,7 +149,10 @@ struct FOG_API Layout : public LayoutItem
 
   //need to be implemented by SubClass!
   virtual IntSize getLayoutSizeHint() const = 0;
-  virtual void add(LayoutItem* item) = 0;
+
+  virtual void add(LayoutItem* item) {
+    item->_withinLayout = this;
+  }
   void add(Widget* widget);
 
   virtual LayoutItem* getAt(int index) const = 0;
@@ -120,14 +163,39 @@ struct FOG_API Layout : public LayoutItem
   virtual void remove(LayoutItem* index);
   virtual int indexOf(LayoutItem*) const;
 
+  void setAlignment(uint32_t alignment) {
+    _alignment = alignment;
+  }
+
+  bool setAlignment(LayoutItem *item, uint32_t alignment)
+  {
+    if(item->_withinLayout == this) {
+      item->_alignment = alignment;
+      invalidateLayout();
+      return true;
+    }
+    
+    return false;
+  }
+
   LayoutItem* _parentItem;
+
+  void addChildLayout(Layout *l);
 
   static bool removeWidgetRecursively(LayoutItem *li, Widget *w);
   void reparentChildWidgets(Widget *mw);
 
+  virtual void onLayout(LayoutEvent* e);
+
+  FOG_EVENT_BEGIN()
+    FOG_EVENT_DEF(EVENT_LAYOUT_REQUEST     , onLayout          , LayoutEvent    , OVERRIDE)    
+  FOG_EVENT_END()
+
 protected:
   bool activate();
   void update();
+  void callSetGeometry(const IntSize& size);
+  void activateRecursiveHelper(LayoutItem *item);
 
   IntMargins _contentmargins;
 
@@ -135,12 +203,13 @@ protected:
   uint _toplevel : 1;
   uint _enabled : 1;
   uint _activated : 1;
-  uint _autoNewChild : 1;
+  uint _margininvalid : 1;
   uint _unused : 4;
   
   SizeConstraint _constraint;
   IntRect _rect;
 
+private:
   FOG_DISABLE_COPY(Layout)
 };
 
---
+++ Fog/Fog/Gui/Layout/LayoutPolicy.h
@@ -0,0 +1,101 @@
+// [Fog-Gui Library - Public API]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Guard]
+#ifndef _FOG_GUI_LAYOUT_LAYOUTPOLICY_H
+#define _FOG_GUI_LAYOUT_LAYOUTPOLICY_H
+
+// [Dependencies]
+#include <Fog/Core/Constants.h>
+#include <Fog/Core/Object.h>
+#include <Fog/Graphics/Geometry.h>
+#include <Fog/Gui/Constants.h>
+//! @addtogroup Fog_Gui
+//! @{
+
+namespace Fog {
+
+// ============================================================================
+// [Forward Declarations]
+// ============================================================================
+
+struct Layout;
+struct Widget;
+struct SpacerItem;
+
+// ============================================================================
+// [Fog::LayoutPolicy]
+// ============================================================================
+
+struct LayoutPolicy {
+  LayoutPolicy(uint32_t policy)  {
+    _data._all = 0;
+    _data._policy = policy;
+  }
+
+  FOG_INLINE uint32_t getHorizontalStretch() const { return _data._horizontalStretch; }
+  FOG_INLINE uint32_t getVerticalStretch() const { return _data._verticalStretch; }
+  FOG_INLINE void setHorizontalStretch(uchar stretchFactor) { _data._horizontalStretch = stretchFactor; }
+  FOG_INLINE void setVerticalStretch(uchar stretchFactor) { _data._verticalStretch = stretchFactor; }
+
+  FOG_INLINE uint32_t getPolicy() const { return _data._policy; }
+  FOG_INLINE uint32_t getHorizontalPolicy() const { return _data._policy & 0xF; }
+  FOG_INLINE uint32_t getVerticalPolicy() const { return _data._policy & 0xF0; }
+  FOG_INLINE void setPolicy(uint32_t policy) { _data._policy = policy; }
+
+
+  FOG_INLINE bool isHorizontalPolicyIgnored() const { return getHorizontalPolicy() == LAYOUT_POLICY_WIDTH_IGNORED; }
+  FOG_INLINE bool isVerticalPolicyIgnored() const { return getVerticalPolicy() == LAYOUT_POLICY_HEIGHT_IGNORED; }
+
+  FOG_INLINE bool isHorizontalPolicyExpanding() const { return getHorizontalPolicy() == LAYOUT_POLICY_WIDTH_EXPANDING; }
+  FOG_INLINE bool isVerticalPolicyExpanding() const { return getVerticalPolicy() == LAYOUT_POLICY_HEIGHT_EXPANDING; }
+
+  uint32_t expandingDirections() const {
+    uint32_t result;
+    if (_data._policy & LAYOUT_EXPANDING_WIDTH)
+      result |= ORIENTATION_HORIZONTAL;
+    if (_data._policy & LAYOUT_EXPANDING_HEIGHT)
+      result |= ORIENTATION_VERTICAL;
+
+    return result;
+  }
+
+  //is this method really usefull?
+  void transpose() {    
+    uint32_t result = (_data._policy << LAYOUT_HEIGHT_SHIFT); //move vertial to horizontal position
+    result |= (_data._policy >> LAYOUT_HEIGHT_SHIFT);
+    _data._policy = (result & 0xFF);
+
+    uint32_t hStretch = uint32_t(getHorizontalStretch());
+    uint32_t vStretch = uint32_t(getVerticalStretch());
+    setHorizontalStretch(vStretch);
+    setVerticalStretch(hStretch);
+  }
+
+
+  FOG_INLINE void setHeightForWidth(bool b) { _data._heightForWidth = b; }
+  FOG_INLINE bool hasHeightForWidth() const { return _data._heightForWidth; }
+
+  bool operator==(const LayoutPolicy& s) const { return _data._all == s._data._all; }
+  bool operator!=(const LayoutPolicy& s) const { return _data._all != s._data._all; }
+
+  union u {
+    struct {
+      uint32_t _policy : 8;  
+      uint32_t _horizontalStretch : 8;
+      uint32_t _verticalStretch : 8;
+
+      uint32_t _heightForWidth : 1;
+      uint32_t _unused : 7;
+    };
+
+    uint32_t _all;
+  } _data;
+
+};
+
+}
+
+#endif
