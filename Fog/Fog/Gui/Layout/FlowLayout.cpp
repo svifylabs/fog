aed605a366378901393eb0050445be59e0bf1461
stefanvt <stefanvt@gmx.de>
2010-04-16 14:41:21 +0000

    - simple flow layout inserted

---
+++ Fog/Fog/Gui/Layout/FlowLayout.cpp
@@ -0,0 +1,168 @@
+// [Fog-Gui Library - Public API]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Precompiled Headers]
+#if defined(FOG_PRECOMP)
+#include FOG_PRECOMP
+#endif // FOG_PRECOMP
+
+// [Dependencies]
+#include <Fog/Gui/Layout/FlowLayout.h>
+#include <Fog/Gui/Widget.h>
+
+FOG_IMPLEMENT_OBJECT(Fog::FlowLayout)
+
+namespace Fog {
+
+  // ============================================================================
+  // [Fog::FlowLayout]
+  // ============================================================================
+
+  FlowLayout::FlowLayout(Widget *parent, int margin, int hSpacing, int vSpacing)
+    : Layout(parent), _hSpace(hSpacing), _vSpace(vSpacing)
+  {
+    setContentsMargins(margin, margin, margin, margin);
+  }
+
+  FlowLayout::FlowLayout(int margin, int hSpacing, int vSpacing) : _hSpace(hSpacing), _vSpace(vSpacing)
+  {
+    setContentsMargins(margin, margin, margin, margin);
+  }
+
+  FlowLayout::~FlowLayout()
+  {
+    LayoutItem *item;
+    while ((item = takeAt(0)))
+      delete item;
+  }
+
+  void FlowLayout::add(LayoutItem *item)
+  {
+    Layout::add(item);
+    _itemList.append(item);    
+  }
+
+  int FlowLayout::horizontalSpacing() const
+  {
+    if (_hSpace >= 0) {
+      return _hSpace;
+    } else {
+      return DEFAULT_LAYOUT_SPACING;
+      //return smartSpacing(QStyle::PM_LayoutHorizontalSpacing);
+    }
+  }
+
+  int FlowLayout::verticalSpacing() const
+  {
+    if (_vSpace >= 0) {
+      return _vSpace;
+    } else {
+      return DEFAULT_LAYOUT_SPACING;
+      //return smartSpacing(QStyle::PM_LayoutVerticalSpacing);
+    }
+  }
+
+  int FlowLayout::getLength() const
+  {
+    return _itemList.getLength();
+  }
+
+  LayoutItem *FlowLayout::getAt(int index) const
+  {
+    if(index < _itemList.getLength())
+      return _itemList.at(index);
+
+    return 0;
+  }
+
+  LayoutItem *FlowLayout::takeAt(int index)
+  {
+    if (index >= 0 && index < _itemList.getLength())
+      return _itemList.take(index);
+    else
+      return 0;
+  }
+
+  uint32_t FlowLayout::getLayoutExpandingDirections() const
+  {
+    return 0;
+  }
+
+  bool FlowLayout::hasLayoutHeightForWidth() const
+  {
+    return true;
+  }
+
+  int FlowLayout::getLayoutHeightForWidth(int width) const
+  {
+    int height = doLayout(IntRect(0, 0, width, 0), true);
+    return height;
+  }
+
+  void FlowLayout::setLayoutGeometry(const IntRect &rect)
+  {
+    Layout::setLayoutGeometry(rect);
+    doLayout(rect, false);
+  }
+
+  IntSize FlowLayout::getLayoutSizeHint() const
+  {
+    return getLayoutMinimumSize();
+  }
+
+  IntSize FlowLayout::getLayoutMinimumSize() const
+  {
+    IntSize size;
+    int len = _itemList.getLength();
+    for(int i=0;i<len;++i) {
+      size = size.expandedTo(_itemList.at(i)->getLayoutMinimumSize());
+    }
+
+    int l = 2* getContentsMargins().left;
+
+    size = size + IntSize(l, l);
+    return size;
+  }
+
+  int FlowLayout::doLayout(const IntRect &rect, bool testOnly) const
+  {    
+    IntMargins margins = getContentsMargins();
+    IntRect effectiveRect = rect.adjusted(+margins.left, +margins.top, -margins.right, -margins.bottom);
+    int x = effectiveRect.x;
+    int y = effectiveRect.y;
+    int lineHeight = 0;
+
+    int len = _itemList.getLength();
+    for(int i=0;i<len;++i) {
+      LayoutItem *item = _itemList.at(i);
+      int spaceX = horizontalSpacing();
+      if (spaceX == -1)
+        spaceX = 0;
+      int spaceY = verticalSpacing();
+      if (spaceY == -1)
+        spaceY = 0;
+
+      int nextX = x + item->getLayoutSizeHint().getWidth() + spaceX;
+      if (nextX - spaceX > effectiveRect.getRight() && lineHeight > 0) {
+        x = effectiveRect.x;
+        y = y + lineHeight + spaceY;
+        nextX = x + item->getLayoutSizeHint().getWidth() + spaceX;
+        lineHeight = 0;
+      }
+
+      if (!testOnly) 
+      {
+        IntRect rect;
+        IntSize tmp = item->getLayoutSizeHint();
+        rect.set(x,y,tmp.w,tmp.h);
+        item->setLayoutGeometry(rect);
+      }
+
+      x = nextX;
+      lineHeight = Math::max<int>(lineHeight, item->getLayoutSizeHint().getHeight());
+    }
+    return y + lineHeight - rect.y + margins.bottom;
+  }
+}
---
+++ Fog/Fog/Gui/Layout/FlowLayout.h
@@ -0,0 +1,57 @@
+// [Fog-Gui Library - Public API]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Guard]
+#ifndef _FOG_GUI_LAYOUT_FLOWLAYOUT_H
+#define _FOG_GUI_LAYOUT_FLOWLAYOUT_H
+
+// [Dependencies]
+#include <Fog/Gui/Layout/Layout.h>
+#include <Fog/Core/List.h>
+
+//! @addtogroup Fog_Gui
+//! @{
+
+namespace Fog {
+
+  // ============================================================================
+  // [Fog::BoxLayout]
+  // ============================================================================
+
+  //! @brief Base class for all layouts.
+  struct FOG_API FlowLayout : public Layout
+  {
+    FOG_DECLARE_OBJECT(FlowLayout, Layout)
+    FlowLayout(Widget *parent, int margin=-1, int hSpacing=-1, int vSpacing=-1);
+    FlowLayout(int margin=-1, int hSpacing=-1, int vSpacing=-1);
+    ~FlowLayout();
+    
+    int horizontalSpacing() const;
+    int verticalSpacing() const;    
+
+    virtual void add(LayoutItem *item);
+
+    virtual IntSize getLayoutSizeHint() const;
+    virtual uint32_t getLayoutExpandingDirections() const;
+    virtual bool hasLayoutHeightForWidth() const;
+    virtual int getLayoutHeightForWidth(int) const;
+    virtual IntSize getLayoutMinimumSize() const;
+
+    virtual int getLength() const; 
+    virtual LayoutItem *getAt(int index) const;
+    virtual LayoutItem *takeAt(int index);
+
+    virtual void setLayoutGeometry(const IntRect &rect);
+
+  private:
+    int doLayout(const IntRect &rect, bool testOnly) const;
+    List<LayoutItem*> _itemList;
+    int _hSpace;
+    int _vSpace;
+  };
+
+}
+
+#endif
