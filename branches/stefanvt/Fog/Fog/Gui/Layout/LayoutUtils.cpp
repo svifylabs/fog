5845c918bf4cf5432d78d12610cd8723fb33af31
stefanvt@gmx.de <stefanvt@gmx.de@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2010-04-23 05:58:15 +0000

    - GridLayout works now, but still needs some improvement here and there

--- branches/stefanvt/Fog/Fog/Gui/Layout/LayoutUtils.cpp
+++ branches/stefanvt/Fog/Fog/Gui/Layout/LayoutUtils.cpp
@@ -20,76 +20,64 @@ namespace Fog {
     if(head == 0) return;
 
     bool grow = avail > used;
-    int remaining = Math::abs(avail - used);
-    int roundingOffset, currentOffset;
-
-    float flexStep, flexSum;
+    int remaining = Math::abs(avail - used);        
 
     FOG_ASSERT(remaining != 0);   //check: is this too hard??
 
     bool set = false;
+    float flexSum = 0;
 
     // Continue as long as we need to do anything
     while (remaining != 0)
     {
       // Find minimum potential for next correction
-      flexStep = WIDGET_MAX_SIZE;
-      flexSum = 0;
+      float flexStep = FLOAT_MAX;
       T* item = head;
-      while(item) {        
+
+      bool breakfrom = true;
+
+      //calculate sum of all flexes and initialize variables!
+      //TODO: can be done on setter so we can save this loop!
+      while(item) {
         LayoutItem::LayoutStruct* child = item->_layoutdata;
 
         if(!set) {
           child->_potential = grow ? child->_max - child->_hint : child->_hint - child->_min;
-          child->_flex = grow ? child->_flex : 1 / child->_flex;
-          child->_offset = 0;
+          child->_flex = grow ? child->_flex : (1 / child->_flex);
+          flexSum += child->_flex;
         }
 
         if (child->_potential > 0) {
-          flexSum += child->_flex;
-          flexStep = Math::min<float>(flexStep, child->_potential / child->_flex);
+          flexStep = Math::min<float>(flexStep, child->_potential / child->_flex);          
         }
 
         item = (T*)child->_next;
       }
+
+      if(flexSum==0)  //should not happen, but just to be sure :-)
+        break;
       
       set = true;
-      // No potential found, quit here
-      if (flexSum == 0) {
-        break;
-      }
-      // Respect maximum potential given through remaining space
-      // The parent should always win in such conflicts.
-      flexStep = Math::min<float>(remaining, flexStep * flexSum) / flexSum;
+      
+      //never take more space than the parent has!
+      flexStep = Math::min<float>(remaining, flexStep * flexSum) / flexSum; //calculate the divisor for this round
 
-
-      // Start with correction
-      roundingOffset = 0;
+      //start with the calculation
       item = head;
-      while(item) {        
+      int roundingOffset = 0;      
+
+      while(item) {
         LayoutItem::LayoutStruct* child = item->_layoutdata;
 
         if (child->_potential > 0)
         {
+          breakfrom = false;
           // Compute offset for this step
-          currentOffset = Math::min<int>(remaining, child->_potential, ::ceil(flexStep * child->_flex));
-
-          // Fix rounding issues
-          roundingOffset += currentOffset - (flexStep * child->_flex);
-          if (roundingOffset >= 1)
-          {
-            roundingOffset -= 1;
-            currentOffset -= 1;
-          }
-
+          int currentOffset = Math::min<int>(remaining, child->_potential, ::ceil(flexStep * child->_flex));
           // Update child status
           child->_potential -= currentOffset;
 
-          if (grow) {
-            child->_offset += currentOffset;            
-          } else {
-            child->_offset -= currentOffset;
-          }
+          child->_offset += grow? currentOffset : -currentOffset;
           used+=child->_offset;
 
           // Update parent status
@@ -97,6 +85,10 @@ namespace Fog {
         }
 
         item = (T*)child->_next;
+      }
+
+      if(breakfrom) {
+        break;
       }
     }
   }
@@ -120,20 +112,16 @@ namespace Fog {
 
   int collapseMargins(int margin1, int margin2, int margin3) {
     int max=0, min=0;
-
     CALC(margin1);
     CALC(margin2);
     CALC(margin3);
-
     return max + min;
   }
 
   int collapseMargins(int margin1, int margin2) {
     int max=0, min=0;
-
     CALC(margin1);
     CALC(margin2);
-
     return max + min;
   }
 
@@ -152,15 +140,12 @@ namespace Fog {
 
     if (collapse)
     {
-      // Add first child
-      gaps += children.at(0)->getContentLeftMargin();
-
       for (int i=1; i<children.getLength(); ++i) {
         gaps += collapseMargins(spacing, children.at(i-1)->getContentRightMargin(), children.at(i)->getContentLeftMargin());
       }
 
       // Add last child
-      gaps += children.at(children.getLength()-1)->getContentRightMargin();
+      gaps += children.at(0)->getContentLeftMargin() + children.at(children.getLength()-1)->getContentRightMargin();
     }
     else
     {
@@ -189,24 +174,19 @@ namespace Fog {
 
     if (collapse)
     {
-      // Add first child
-      gaps += children.at(0)->getContentLeftMargin();
-
-      for (int i=1; i<children.getLength(); ++i) {
+      for (int i=1; i<children.getLength(); ++i) 
+      {
         gaps += collapseMargins(spacing, children.at(i-1)->getContentBottomMargin(), children.at(i)->getContentTopMargin());
       }
-
-      // Add last child
-      gaps += children.at(children.getLength()-1)->getContentBottomMargin();
+      gaps += children.at(0)->getContentLeftMargin() + children.at(children.getLength()-1)->getContentBottomMargin();
     }
     else
     {
-      // Simple adding of all margins
-      for (int i=1; i<children.getLength(); ++i) {
-        gaps += children.at(i)->getContentTopMargin() + children.at(i)->getContentBottomMargin();
+      for (int i=1; i<children.getLength(); ++i) 
+      {
+        LayoutItem* item = children.at(i);
+        gaps += item->getContentTopMargin() + item->getContentBottomMargin();
       }
-
-      // Add spacing
       gaps += (spacing * (children.getLength()-1));
     }
 
