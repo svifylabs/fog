33b529a6c0711740321ff40d8cafbb979678c9c9
niels.pfeffer <niels.pfeffer@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2010-07-27 16:14:25 +0000

    Corrected formatting

--- trunk/Fog/Fog/Core/MacUtil.h
+++ trunk/Fog/Fog/Core/MacUtil.h
@@ -24,102 +24,113 @@ namespace Fog {
 template <typename T>
 struct RemovePointer
 {
-    typedef T type;
+  typedef T type;
 };
 
 template <typename T>
 struct RemovePointer<T*>
 {
-    typedef T type;
+  typedef T type;
 };
 
 template <typename T>
-struct RetainPtr {
-    typedef typename RemovePointer<T>::type ValueType;
-    typedef ValueType* PtrType;
+struct RetainPtr
+{
+  typedef typename RemovePointer<T>::type ValueType;
+  typedef ValueType* PtrType;
 
-    RetainPtr() : m_ptr(0) {}
-    RetainPtr(PtrType ptr) : m_ptr(ptr) { if (ptr) CFRetain(ptr); }
-    RetainPtr(const RetainPtr& o) : m_ptr(o.m_ptr) { if (PtrType ptr = m_ptr) CFRetain(ptr); }
-    ~RetainPtr() { if (PtrType ptr = m_ptr) CFRelease(ptr); }
-    template <typename U> RetainPtr(const RetainPtr<U>& o) : m_ptr(o.get()) { if (PtrType ptr = m_ptr) CFRetain(ptr); }
+  RetainPtr() : _ptr(0) {}
+  RetainPtr(PtrType ptr) : _ptr(ptr) { if (ptr) CFRetain(ptr); }
+  RetainPtr(const RetainPtr& o) : _ptr(o._ptr) { if (PtrType ptr = _ptr) CFRetain(ptr); }
+  ~RetainPtr() { if (PtrType ptr = _ptr) CFRelease(ptr); }
+  template <typename U> RetainPtr(const RetainPtr<U>& o) : _ptr(o.get()) { if (PtrType ptr = _ptr) CFRetain(ptr); }
 
-    PtrType get() const { return m_ptr; }
-    PtrType releaseRef() { PtrType tmp = m_ptr; m_ptr = 0; return tmp; }
+  PtrType get() const { return _ptr; }
+  PtrType releaseRef() { PtrType tmp = _ptr; _ptr = 0; return tmp; }
 
-    PtrType operator->() const { return m_ptr; }
-    bool operator!() const { return !m_ptr; }
-    //! @brief  This conversion operator allows implicit conversion to bool but not to other integer types.
-    typedef PtrType RetainPtr::*UnspecifiedBoolType;
-    operator UnspecifiedBoolType() const { return m_ptr ? &RetainPtr::m_ptr : 0; }
+  PtrType operator->() const { return _ptr; }
+  bool operator!() const { return !_ptr; }
+  
+  //! @brief  This conversion operator allows implicit conversion to bool but not to other integer types.
+  typedef PtrType RetainPtr::*UnspecifiedBoolType;
+  operator UnspecifiedBoolType() const { return _ptr ? &RetainPtr::_ptr : 0; }
 
-    RetainPtr& operator=(const RetainPtr&);
-    template <typename U> RetainPtr& operator=(const RetainPtr<U>&);
-    RetainPtr& operator=(PtrType optr)
-    {
-	    if (optr)
-	        CFRetain(optr);
-	    PtrType ptr = m_ptr;
-	    m_ptr = optr;
-	    if (ptr)
-	        CFRelease(ptr);
-	    return *this;
-	
-    }
-    template <typename U> RetainPtr& operator=(U*);
+  RetainPtr& operator=(const RetainPtr&);
+
+  template <typename U>
+  RetainPtr& operator=(const RetainPtr<U>&);
+
+  RetainPtr& operator=(PtrType optr)
+  {
+    if (optr)
+      CFRetain(optr);
+    PtrType ptr = _ptr;
+    _ptr = optr;
+    if (ptr)
+      CFRelease(ptr);
+    return *this;
+  
+  }
+  template <typename U>
+  RetainPtr& operator=(U*);
 
 private:
-    PtrType m_ptr;
+  PtrType _ptr;
 };
 
-template <typename T> inline RetainPtr<T>& RetainPtr<T>::operator=(const RetainPtr<T>& o)
+template <typename T>
+FOG_INLINE RetainPtr<T>& RetainPtr<T>::operator=(const RetainPtr<T>& o)
 {
-    PtrType optr = o.get();
-    if (optr)
-        CFRetain(optr);
-    PtrType ptr = m_ptr;
-    m_ptr = optr;
-    if (ptr)
-        CFRelease(ptr);
-    return *this;
+  PtrType optr = o.get();
+  if (optr)
+    CFRetain(optr);
+  PtrType ptr = _ptr;
+  _ptr = optr;
+  if (ptr)
+    CFRelease(ptr);
+  return *this;
 }
 
-/*
-template <class T> inline void swap(RetainPtr<T>& a, RetainPtr<T>& b)
+template <class T>
+FOG_INLINE void swap(RetainPtr<T>& a, RetainPtr<T>& b)
 {
-    a.swap(b);
+  a.swap(b);
 }
 
+
+// TODO For any unknown reason, these compare operators hide the string compare 
+// operators
+#if 0
 template <typename T, typename U> inline bool operator==(const RetainPtr<T>& a, const RetainPtr<U>& b)
 { 
-    return a.get() == b.get(); 
+  return a.get() == b.get(); 
 }
 
 template <typename T, typename U> inline bool operator==(const RetainPtr<T>& a, U* b)
 { 
-    return a.get() == b; 
+  return a.get() == b; 
 }
 
 template <typename T, typename U> inline bool operator==(T* a, const RetainPtr<U>& b) 
 {
-    return a == b.get(); 
+  return a == b.get(); 
 }
 
 template <typename T, typename U> inline bool operator!=(const RetainPtr<T>& a, const RetainPtr<U>& b)
 { 
-    return a.get() != b.get(); 
+  return a.get() != b.get(); 
 }
 
 template <typename T, typename U> inline bool operator!=(const RetainPtr<T>& a, U* b)
 {
-    return a.get() != b; 
+  return a.get() != b; 
 }
 
 template <typename T, typename U> inline bool operator!=(T* a, const RetainPtr<U>& b)
 { 
-    return a != b.get(); 
+  return a != b.get(); 
 }
-*/
+#endif
 
 // =================================================================================
 // [Carbon and Cocoa String Conversions]
