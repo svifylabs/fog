1837f024702b2e6fe9eb470e8853f921e162ac13
kobalicek.petr@gmail.com <kobalicek.petr@gmail.com@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2012-02-27 10:18:46 +0000

    Fixed bug in property system

--- trunk/Fog/Src/Fog/Core/Dom/Dom.h
+++ trunk/Fog/Src/Fog/Core/Dom/Dom.h
@@ -517,7 +517,7 @@ struct FOG_API DomElement : public DomContainer
 
 #define FOG_DOM_ELEMENT_INIT() \
   FOG_MACRO_BEGIN \
-    _objectPropertyCount = Self::_PROPERTY_TOTAL; \
+    _objectPropertyCount = Self::_PROPERTY_COUNT; \
   FOG_MACRO_END
 
 // ============================================================================
