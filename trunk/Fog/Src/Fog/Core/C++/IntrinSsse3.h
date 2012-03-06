d0146a57971805a96c281b7329037192b77696e0
kobalicek.petr <kobalicek.petr@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2012-03-06 08:31:44 +0000

    Added correct IntrinXXX files.

---
+++ trunk/Fog/Src/Fog/Core/C++/IntrinSsse3.h
@@ -0,0 +1,17 @@
+// [Fog-Core]
+//
+// [License]
+// MIT, See COPYING file in package
+
+// [Guard]
+#ifndef _FOG_CORE_CPP_INTRINSSSE3_H
+#define _FOG_CORE_CPP_INTRINSSSE3_H
+
+// [Dependencies]
+#include <Fog/Core/C++/Base.h>
+#include <Fog/Core/C++/IntrinSse3.h>
+
+#include <tmmintrin.h>
+
+// [Guard]
+#endif // _FOG_CORE_CPP_INTRINSSSE3_H
