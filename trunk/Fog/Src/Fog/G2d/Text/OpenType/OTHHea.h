b98c1bfe2b2c7726f72562af7b711349127365d0
kobalicek.petr <kobalicek.petr@575ff6b6-1f5b-11de-a108-cd2f117ce590>
2012-03-08 16:31:44 +0000

    Added 'hhea'.

--- trunk/Fog/Src/Fog/G2d/Text/OpenType/OTHHea.h
+++ trunk/Fog/Src/Fog/G2d/Text/OpenType/OTHHea.h
@@ -26,10 +26,43 @@ namespace Fog {
 
 struct FOG_NO_EXPORT OTHHeaHeader
 {
+  //! @brief Version number in fixed point (0x00010000 for 1.0).
+  OTFixed16x16 version;
+  //! @brief Typographic ascent.
+  OTInt16 ascender;
+  //! @brief Typographic descent.
+  OTInt16 descender;
+  //! @brief Typographic line-gap.
+  OTInt16 lineGap;
+  //! @brief Maximum advance width value in 'htmx' table.
+  OTUInt16 advanceWidthMax;
+  //! @brief Minimum left sidebearing value in 'htmx' table.
+  OTInt16 minLeftSideBearing;
+  //! @brief Minimum right sidebearing value in 'htmx' table.
+  //!
+  //! @note Calculated as "min(aw - lsb - (xMax - xMin))".
+  OTInt16 minRightSideBearing;
+  //! @brief Maximum x extent.
+  //!
+  //! @note Calculated as "max(lsb + (xMax - xMin))".
+  OTInt16 xMaxExtent;
+  //! @brief Used to calculate the slope of the cursor (rise).
+  OTInt16 caretSlopeRise;
+  //! @brief Used to calculate the slope of the cursor (run).
+  OTInt16 caretSlopeRun;
+  //! @brief The amount by which a slanted highlight on a glyph needs to be 
+  //! shifted to produce the best appearance. Set to 0 for non-slanted fonts.
+  OTInt16 caretOffset;
+  //! @brief Reserved, should be zero.
+  OTInt16 reserved[4];
+  //! @brief Metric data format (should be zero).
+  OTInt16 metricDataFormat;
+  //! @brief Number of hMetric entries in 'hmtx' table.
+  OTUInt16 numberOfHMetrics;
 };
 
 // ============================================================================
-// [Fog::OTHmtx]
+// [Fog::OTHHea]
 // ============================================================================
 
 struct FOG_NO_EXPORT OTHHea : public OTTable
